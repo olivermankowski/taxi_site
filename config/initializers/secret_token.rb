5dd13c7c27f5a75bbf97922a453343751fb06d01
Oliver Mankowski <olivermankowski@gmail.com>
2011-01-06 02:08:27 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaxiSite::Application.config.secret_token = '7dd758484a5336731ccf458b9832218a5d8cf5377fd76306fee0bf0653d1c2dce341b4c3569f0168ec0fa704c85a07edbd29e1c0fe1c9bc3ffd47ce56621fb95'
